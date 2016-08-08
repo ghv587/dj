23af7e3f5cbe104dff8c6416956f85f1459eb8cc
ghv587 <zhang_zhenpeng@163.com>
2016-08-08 08:05:35 +0800

    blog

--- weibo/blog/models.py
+++ weibo/blog/models.py
@@ -1,5 +1,17 @@
+#coding=utf-8
+
 from __future__ import unicode_literals
 
 from django.db import models
 
 # Create your models here.
+
+#user (yonghu)
+class User(models.Model):
+    username = models.CharField(max_length=30, verbose_name="yonghu")
+
+    class Meta:
+        verbose_name = "yonghu"
+        verbose_name_plural = verbose_name
+
+
